a78f0084af8df413afafe1966492bb72c0ebfae4
benhirsch24 <benhirsch24@gmail.com>
2019-01-18 21:52:11 -0800

    What's on tap?
    * Main now scans the dynamodb table and returns the names of each beer
      which is currently on tap.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This is copied from [softprop/lambda-rust](https://github.com/softprops/lambda-r
 
 This uses the lambci build-provided docker image to provide the environment to build the binary in so there's no complicated building with different toolchains and downloading cross-compiler steps.
 
-Also note that I'm running this with Docker for Mac: I had to change the DNS settings for Docker... It was set to 10.4.4.10, I changed the settings (Docker icon in the toolbar -> Preferences -> Daemon -> Advanced -> DNS in the JSON) to 8.8.8.8 and 8.8.4.4. Then I was able to get internet access and build the image as well as access the internet for Cargo Crates.
+Also note that I'm running this with Docker for Mac: I had to change the DNS settings for Docker... It was set to 10.4.4.10, I changed the settings (Docker icon in the toolbar -> Preferences -> Daemon -> Advanced -> DNS in the JSON) to 8.8.8.8 and 8.8.4.4 (OpenDNS). Then I was able to get internet access and build the image as well as access the internet for Cargo Crates.
 
 # To build the image
 
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate lambda_runtime as lambda;
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
 extern crate log;
 extern crate rusoto_core;
 extern crate rusoto_dynamodb;
@@ -9,19 +10,23 @@ extern crate simple_logger;
 
 use lambda::error::HandlerError;
 use rusoto_core::{HttpClient, Region};
-use rusoto_credential::{EnvironmentProvider};
+use rusoto_credential::{DefaultCredentialsProvider};
 use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ScanInput};
 use std::error::Error;
 
 #[derive(Deserialize, Clone)]
 struct SlackInput {
-    #[serde(rename = "text")]
-    text: String,
 }
 
 #[derive(Serialize, Clone)]
 struct SlackOutput {
     message: String,
+}
+
+impl SlackOutput {
+    pub fn with_message(msg: String) -> SlackOutput {
+        SlackOutput { message: msg }
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -31,40 +36,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn my_handler(e: SlackInput, _: lambda::Context) -> Result<SlackOutput, HandlerError> {
-    let ddb_client = DynamoDbClient::new_with(HttpClient::new().unwrap(), EnvironmentProvider::default(), Region::UsWest2);
+fn my_handler(_: SlackInput, _: lambda::Context) -> Result<SlackOutput, HandlerError> {
+    let ddb_client = DynamoDbClient::new_with(HttpClient::new().unwrap(),
+                                              DefaultCredentialsProvider::new().unwrap(),
+                                              Region::UsWest2);
     let mut scan_input = ScanInput::default();
     scan_input.table_name = "Beers".to_string();
-    match ddb_client.scan(scan_input).sync() {
-        Ok(o) => {
-            match o.items {
-                Some(is) => {
-                    for row in is {
-                        for (key, val) in &row {
-                            if *key != "IsOnTap".to_string() {
-                                println!("Key: {} Value: {}", key, val.s.as_ref().unwrap());
-                            } else {
-                                println!("Key: {} Value: {}", key, val.n.as_ref().unwrap());
-                            }
-                        }
-                    }
-                },
-                None => println!("No items")
-            };
-        },
+    let response = match ddb_client.scan(scan_input).sync() {
         Err(e) => {
-            match e {
-                rusoto_dynamodb::ScanError::Unknown(ref resp) => {
-                    println!("Unknown error: {:?}", e);
-                    println!("Body: {}", std::str::from_utf8(&resp.body).unwrap());
+            error!("Error with dynamodb scan {:?}", e);
+            "It looks like there's no beers on tap right now.".to_string()
+        },
+        Ok(row) => {
+            match row.items {
+                Some(items) => {
+                    let names = items.iter()
+                        .map(|r| r.get("Name").as_ref().unwrap().s.as_ref().unwrap().clone())
+                        .fold(String::new(), |acc, name| format!("{}\n{}", acc, name));
+                    format!("Current beers on tap:{}", names)
                 },
-                _ => {
-                    dbg!(e);
-                }
+                None => "It looks like there's no beers on tap right now.".to_string()
             }
         }
     };
-    Ok(SlackOutput {
-        message: format!("Hello, {}!", e.text),
-    })
+    Ok(SlackOutput::with_message(response))
 }
